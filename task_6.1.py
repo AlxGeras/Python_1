ab9d07c89d7d77b4ec7fad44adce407c34fcbf79
Gera <alxgeras@mail.ru>
2021-01-25 13:21:48 +0300

    Исправления

--- task_6.1.py
+++ task_6.1.py
@@ -2,5 +2,9 @@ from itertools import count
 
 n = int(input('С какого числа генерировать? '))
 
+k = int(input('На каком элементе прервать генерацию? '))
+
 for el in count(n):
     print(el)
+    if el == k:
+        break
